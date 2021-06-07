12ddf1284df0899001a713265634b392776dd2e1
vaidehi vaniya <85460731+vaidehi0816@users.noreply.github.com>
2021-06-07 07:27:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+   return (
+     <div className="note">
+       <h1>Javascript and React.js</h1>
+       <P>The bootcamp was an amazing. its important and helpfull to the person whos started to learning coding.
+       </P> 
+     </div>
+   );
+ }
